72b25d932731fcd9a6bf16535867657e279579e0
Jon Mah <jonmah@MLT077569.local>
2020-07-03 09:24:30 -0700

    User joining room with welcome messages

--- server/index.js
+++ server/index.js
@@ -13,10 +13,24 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 io.on('connection', socket => {
-  console.log('We have a new connection')
-  socket.on('join', ({ name, room }) => {
-    console.log(name, room)
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room })
+
+    if (error) return callback(error)
+
+    // User gets a message when entering the room
+    socket.emit('message', {
+      user: 'admin',
+      text: `Welcome, ${user.name} to the room ${user.room}!`
+    })
+    // Room gets a message when User enters the room
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined!` })
+    socket.join(user.room)
+    callback()
   })
+
   socket.on('disconnect', () => {
     console.log('User has left')
   })

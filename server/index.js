58ec97c466b73c27d487197d91f5565694e1bd67
Jon Mah <jonmah@MLT077569.local>
2020-07-03 11:01:13 -0700

    Emit send message and capturing messages in Chat

--- server/index.js
+++ server/index.js
@@ -31,6 +31,12 @@ io.on('connection', socket => {
     callback()
   })
 
+  socket.on('sendMessage', ({ message }, callback) => {
+    const user = getUser(socket.id)
+    io.to(user.room).emit('message', { user: user.name, text: message })
+    callback()
+  })
+
   socket.on('disconnect', () => {
     console.log('User has left')
   })

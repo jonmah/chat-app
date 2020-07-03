b3dc291ef9c199b27aeddc6f0700a493fc24ee05
Jon Mah <jonmah@MLT077569.local>
2020-07-03 04:17:18 -0700

    Chat gets querystrings and socket.emit() to server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => <h1>Chat</h1>
+let socket
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'locahost:5000'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+
+    setName(name)
+    setRoom(room)
+
+    socket.emit('Join', { name, room })
+    console.log(socket)
+  }, [ENDPOINT, location.search])
+
+  return <h1>Chat</h1>
+}
 
 export default Chat

8787cb125dee9e88cef572aff4b8e96546e32ac3
Jon Mah <jonmah@MLT077569.local>
2020-07-03 03:45:03 -0700

    Building out Join

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,5 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-const Join = () => <h1>Join</h1>
+const JoinOuterContainer = styled.div``
+const JoinInnerContainer = styled.div``
+
+const Join = () => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  return (
+    <JoinOuterContainer>
+      <JoinInnerContainer>
+        <h1>Join</h1>
+        <div>
+          <input
+            type='text'
+            placeholder=''
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            placeholder=''
+            onChange={e => setRoom(e.target.value)}
+          />
+        </div>
+        <Link
+          onClick={e => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button type='submit'>Sign In</button>
+        </Link>
+      </JoinInnerContainer>
+      Join
+    </JoinOuterContainer>
+  )
+}
 
 export default Join
